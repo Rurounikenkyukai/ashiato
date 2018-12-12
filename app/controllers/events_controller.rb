a192516a57bfe3b69239b00fec59f751a58525e6
killing47 <whipple.box47@gmail.com>
2018-12-12 01:27:19 +0000

    ユーザ周り

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,5 @@
 class EventsController < ApplicationController
     def index
-        binding.pry
         @event = Event.new
         if params["events_id"] == nil
             @events = Event.page(params[:page]).reverse_order
@@ -19,33 +18,32 @@ class EventsController < ApplicationController
     end
 
   	def create
-	end
+	  end
 
-	def update
-	end
+  	def update
+	  end
 
-	def destroy
-	end
+	  def destroy
+	  end
 
-  def search
-    if params[:happy] == "1"
-      cd = Cd.where(params[:content])
-    elsif params[:happy] =="2"
-      event = Event.where(params[:content])
-    else
-      artist = Artist.where(params[:content])
+    def search
+    ##  if params[:happy] == "1"
+      ##  @cd = Cd.where(params[:content])
+      ##elsif params[:happy] =="2"
+        @events = Event.where(params[:content])
+      ##else
+      ##  @artist = Artist.where(params[:content])
+    ##  end
     end
-  end
 
-  def event_search
-    event_day = params[:event][:event_day]
-    event_city = params[:event][:event_city]
-    @events = Event.where(event_day: event_day).where(event_city: event_city)
-    events_id = []
-    @events.each do |a|
+    def event_search
+      event_day = params[:event][:event_day]
+      event_city = params[:event][:event_city]
+      @events = Event.where(event_day: event_day).where(event_city: event_city)
+      events_id = []
+      @events.each do |a|
         event_id.push(a.id)
+      end
+      redirect_to events_path(events_id: events_id)
     end
-    redirect_to events_path(events_id: events_id)
-  end
-
 end
