484518e052e78cc07e2bbd63853afa2d9eacbe2e
takenyan2 <takenyan314@gmail.com>
2018-12-13 04:53:12 +0000

    イベント検索

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,18 +1,15 @@
 class EventsController < ApplicationController
     def index
-        if params[:id].nil?
+          if params[:events_id].nil?
             @event = Event.new
             @events = Event.page(params[:page]).reverse_order
-            binding.pry
         else
             @event = Event.new
-            event_day = params[:event][:event_day]
-            event_city = params[:event][:event_city]
-            @events = Event.where(event_day: event_day).where(event_city: event_city)
-            events_id = []
-            @events.each do |event|
-            event_id.push(event.id)
+            @events_array = []
+            params[:events_id].each do |a|
+            @events_array.push(Event.find(a))
             end
+            @events = Kaminari.paginate_array(@events_array).page(params[:page])
         end
     end
 
@@ -22,17 +19,35 @@ class EventsController < ApplicationController
     def edit
     end
 
-  	def create
-	  end
+    def create
+    end
 
-  	def update
-	  end
+    def update
+    end
 
-	  def destroy
-	  end
+    def destroy
+    end
+
+    def search
+    ##  if params[:happy] == "1"
+      ##  @cd = Cd.where(params[:content])
+      ##elsif params[:happy] =="2"
+        @events = Event.where(params[:content])
+      ##else
+      ##  @artist = Artist.where(params[:content])
+    ##  end
+    end
+
 
      def event_search
-        
+        event_day = params[:event][:event_day]
+        event_city = params[:event][:event_city]
+        @events = Event.where(event_day: event_day).where(event_city: event_city)
+        events_id = []
+        @events.each do |event|
+        events_id.push(event.id)
+        end
+        redirect_to events_path(events_day: events_id)
     end
 
 end
