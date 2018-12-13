63a2e2a4d8f727facdbe6910ffc449540db5acc4
takenyan2 <takenyan314@gmail.com>
2018-12-13 02:31:32 +0000

    test1

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '03ebd6a13a584cf8e80ef4c4f932857a1cc95712287a00f37b6d96dcd5dac765ba154d5ac119834a902d185579b50a7a9db722e2c202bed3f073170085556942'
