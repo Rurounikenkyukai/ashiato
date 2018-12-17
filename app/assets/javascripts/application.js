a013627910d5057123355474d792de466416808e
kakeke <dedael2525@gmail.com>
2018-12-17 16:06:38 +0900

    [update]カート機能、イベント編集機能

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,3 +30,15 @@ $(window).scroll( function() {
            }
      }
 });
+
+$(function() {
+  $('[name="btn"]:radio').change( function() {
+    if($('[id=a]').prop('checked')){
+      $('.text').fadeOut();
+      $('.text01').fadeIn();
+    } else if ($('[id=b]').prop('checked')) {
+      $('.text').fadeOut();
+      $('.text02').fadeIn();
+    } 
+  });
+});
