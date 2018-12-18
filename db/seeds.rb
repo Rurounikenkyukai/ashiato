948977f9b2f9b8d472d7098f58d4718ee9c1df9e
takenyan2 <takenyan314@gmail.com>
2018-12-18 08:30:54 +0000

    CD詳細

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: "aaa3@aaa", first_name: "性", last_name: "名", kana_first_name: "セイ", kana_last_name: "メイ", nick_name: "あだ名", postal_code: "0000000", address: "東京都", user_image_id: "0", deleted_at: nil, admin: true, password: "aaaaaa" )
