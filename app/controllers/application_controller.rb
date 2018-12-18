eebebd4e9dc6a2ae48bd9069bd9e2cc242452540
killing47 <whipple.box47@gmail.com>
2018-12-18 06:43:42 +0000

    検索完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,7 @@
 class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :cart_count
-  def cart_count
-     if current_user
-      @count = current_user.cart_items.length
-    end
-  end
 
 
 
-  protected
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :kana_first_name, :kana_last_name, :nick_name, :postal_code, :address, :phone_number])
-  end
 end
--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -3,18 +3,5 @@ class CartItemsController < ApplicationController
   def show
   end
 
-  def create
-  	cart_items = CartItem.new(cart_item_params)
-  	cart_items.save
-  	redirect_to cart_item_buy_path
-  end
-
-  def destroy
-  end
-
-private
-	def cart_item_params
-		params.require(:cart_item).permit(:cd_quantity, :cd_id, :user_id)
-	end
-
+  
 end
