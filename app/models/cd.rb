ea9d51a0e32a5ee607dc84500aa3b4178fbf6c3e
kakeke <dedael2525@gmail.com>
2019-01-03 20:32:19 +0900

    調整

--- app/models/cd.rb
+++ app/models/cd.rb
@@ -6,6 +6,7 @@ class Cd < ApplicationRecord
 	has_many :recoding_artists
 	has_many :artists, through: :recoding_artists
 	accepts_nested_attributes_for :discs, allow_destroy: true
+	accepts_nested_attributes_for :recoding_artists, allow_destroy: true
 	validates :cd_title, presence: true
 	validates :cd_genre, presence: true, length: { maximum:15 }
 	validates :cd_label, presence: true, length: { maximum:25 }
